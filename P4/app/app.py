22baab8e2f23f08ae1d64d74e3830a0476700353
patriciacorhid <patriciacorhid@gmail.com>
2020-11-18 20:42:25 +0100

    A falta de modificar

--- P4/app/app.py
+++ P4/app/app.py
@@ -24,16 +24,18 @@ def lista():
     movies = db.video_movies.find() # devuelve un cursor(*), no una lista ni un iterador
 
     lista_movies = []
-    i = 0
 
     for t in movies:
-        if i< 20:
-            app.logger.debug(t) # salida consola
-            lista_movies.append(t)
-            i+=1
+        app.logger.debug(t) # salida consola
+        lista_movies.append(t)
 
     # a los templates de Jinja hay que pasarle una lista, no el cursor
-    return render_template('lista.html', movies=lista_movies)
+    return render_template('lista.html', movies=lista_movies, buscar = False)
+
+# Buscar película
+@app.route('/buscar')
+def buscar():
+    return render_template('busqueda.html')
 
 # Buscar pelicula por titulo
 @app.route('/title',methods=['POST'])
@@ -44,10 +46,11 @@ def buscar_titulo():
   # Si el nombre es vacío, muestro mensaje de error
   if mtitle == '':
       flash('Película inválida')
-      return redirect(url_for('mongo'))
+      return redirect(url_for('buscar'))
 
-  lista = db.video_movies.find({"title": mtitle})
-  return render_template('lista.html', movies=lista)
+  lista = db.video_movies.find({"title": {"$regex":mtitle}})
+  #lista = db.video_movies.find({"title": mtitle})
+  return render_template('lista.html', movies=lista, buscar = True)
 
 # Buscar pelicula por titulo
 @app.route('/year',methods=['POST'])
@@ -58,11 +61,11 @@ def buscar_year():
   # Si el nombre es vacío, muestro mensaje de error
   if myear == '':
       flash('Película inválida')
-      return redirect(url_for('mongo'))
+      return redirect(url_for('buscar'))
 
 
   lista = db.video_movies.find({"year": int(myear)})
-  return render_template('lista.html', movies=lista)
+  return render_template('lista.html', movies=lista, buscar = True)
 
 # Buscar pelicula por titulo
 @app.route('/imdb',methods=['POST'])
@@ -73,10 +76,10 @@ def buscar_imdb():
   # Si el nombre es vacío, muestro mensaje de error
   if mimdb == '':
       flash('Película inválida')
-      return redirect(url_for('mongo'))
+      return redirect(url_for('buscar'))
 
-  lista = db.video_movies.find({"imdb": mimdb})
-  return render_template('lista.html', movies=lista)
+  lista = db.video_movies.find({"imdb": {"$regex":mimdb}})
+  return render_template('lista.html', movies=lista, buscar = True)
 
 
 # Buscar pelicula por titulo
@@ -88,7 +91,103 @@ def buscar_tipo():
   # Si el nombre es vacío, muestro mensaje de error
   if mtype == '':
       flash('Película inválida')
-      return redirect(url_for('mongo'))
+      return redirect(url_for('buscar'))
 
-  lista = db.video_movies.find({"type": mtype})
-  return render_template('lista.html', movies=lista)
+  lista = db.video_movies.find({"type": {"$regex":mtype}})
+  return render_template('lista.html', movies=lista, buscar = True)
+
+# Añadir película acceso a formulario
+@app.route('/insertar')
+def insertar():
+    return render_template('insertar.html')
+
+# Añadir pelicula recoger datos de formulario
+@app.route('/insert',methods=['POST'])
+def insert():
+    # Pido datos del formulario
+    mtitle = request.form['title']
+    myear = request.form['year']
+    mimdb = request.form['imdb']
+    mtype = request.form['type']
+
+    # Compruebo que todos los campos estén rellenos
+    if mtitle == '' or myear == '' or mimdb == '' or mtype == '':
+        flash('Película inválida')
+        return redirect(url_for('insertar'))
+
+    # Compruebo que la película no esté ya en la BD
+    movies = db.video_movies.find({'title': mtitle, 'year': int(myear), 'imdb': mimdb, 'type': mtype})
+    lista_movies = []
+
+    for t in movies:
+        app.logger.debug(t) # salida consola
+        lista_movies.append(t)
+
+    # Si ya estaba, devuelvo mensaje de error
+    if(len(lista_movies)>0):
+        flash('Película inválida. Ya existe en la base de datos.')
+        return redirect(url_for('insertar'))
+
+    # Si no estaba la inserto
+    db.video_movies.insert({'title': mtitle, 'year': int(myear), 'imdb': mimdb, 'type': mtype})
+    return render_template('insertar.html')
+
+# Modificar película acceso a formulario
+@app.route('/modificar')
+def modificar():
+    return render_template('modificar.html')
+
+# Busco la película a modificar
+@app.route('/mod_title',methods=['POST'])
+def modify():
+    # Pido datos del formulario
+    mtitle = request.form['title']
+
+    # Compruebo que el campo no es vacía
+    if mtitle == '':
+        flash('Película inválida')
+        return redirect(url_for('modificar'))
+
+    # Compruebo que la película esté ya en la BD
+    movies = db.video_movies.find({'title': mtitle})
+    lista_movies = []
+
+    for t in movies:
+        app.logger.debug(t) # salida consola
+        lista_movies.append(t)
+
+    # Si no está, devuelvo mensaje de error
+    if(len(lista_movies)==0):
+        flash('Película inválida.')
+        return redirect(url_for('modificar'))
+
+    return render_template('modify.html', movie=lista_movies[0])
+
+# Modificar pelicula
+@app.route('/mod',methods=['POST'])
+def modif():
+    # Pido datos del formulario
+    mtitle = request.form['title']
+    myear = request.form['year']
+    mimdb = request.form['imdb']
+    mtype = request.form['type']
+
+    # Si no estaba la inserto
+    db.video_movies.update({'title': mtitle}, {"$set":{'year': int(myear), 'imdb': mimdb, 'type': mtype}})
+    return redirect(url_for('modificar'))
+
+# Borrar pelicula formulario
+@app.route('/borrar')
+def borrar():
+    return render_template('borrado.html')
+
+# Borrar pelicula
+@app.route('/borrado',methods=['POST'])
+def borrado():
+    # Pido datos del formulario
+    mtitle = request.form['title']
+
+    # Borro la película
+    db.video_movies.delete_one({'title': mtitle})
+    return redirect(url_for('borrar'))
+
